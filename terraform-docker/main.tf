47fc1c9451de60d98b6c761bc3d7f6b74fce7865
Marquise-Wheeler <mardanmw27+LUIT@gmail.com>
2022-08-03 13:58:57 +0000

    Added gitignore

---
+++ terraform-docker/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.20"
+    }
+  }
+}
+
+# download nodered image
+
+provider "docker" {}
+
+resource "null_resource" "dockervol" {
+  provisioner "local-exec" {
+    command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
+  }
+}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
+
+resource "random_string" "random" {
+  count   = var.container_count
+  length  = 4
+  special = false
+  upper   = false
+}
+
+
+
+resource "docker_container" "nodered_container" {
+  count = var.container_count
+  #  name  = "nodered"
+  name  = join("-", ["nodered", random_string.random[count.index].result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = var.int_port
+    external = var.ext_port
+  }
+  volumes {
+    container_path = "/data"
+    host_path = "/home/ubuntu/environment/terraform-docker/noderedvol"
+  }
+}
+
+
+
+
+
+
