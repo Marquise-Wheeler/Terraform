76a76d0dd0fd996f0cff8bad0e87e4e0826560d1
Marquise-Wheeler <mardanmw27+LUIT@gmail.com>
2023-10-19 21:35:04 -0500

    Change AMI

--- web-server/main.tf
+++ web-server/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0df435f331839b2d6"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
  
