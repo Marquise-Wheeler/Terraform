b30273d38ebf2baecb5dba35c6a4ad9fb13c5ad4
Marquise-Wheeler <mardanmw27+LUIT@gmail.com>
2023-10-19 21:15:30 -0500

    Change AZ

--- web-server/main.tf
+++ web-server/main.tf
@@ -10,14 +10,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
-
+ 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
