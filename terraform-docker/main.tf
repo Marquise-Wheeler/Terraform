eea51d05d9e7e55977f2248b9ccfcdfbb2badee1
mardanmw27+LUIT@gmail.com <mardanmw27+LUIT@gmail.com>
2024-03-24 03:26:50 +0000

    Make changes to main.tf and add Commands.txt

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,14 +1,29 @@
 terraform {
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
+      source = "kreuzwerker/docker"
+      version = "~> 2.7"
+
     }
   }
 }
-# PROVIDER ENTRY
+
+
 provider "docker" {}
+
+# download nodered image
 
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
+}
+
+# start the container
+
+resource "docker_container" "nodered_container" {
+  name  = "nodered"
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    external = 1880
+  }
 }
